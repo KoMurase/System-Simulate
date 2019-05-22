f1fdfedca25f65286fa587366064c45d5e87ebe8
Ko Murase <mkou4869@icloud.com>
2019-05-22 11:46:19 +0900

    'sendamessageinclass'

---
+++ No.3/count_method.py
@@ -0,0 +1,8 @@
+import collections
+l=['a','a','a','b','c','c']
+print(l.count('a'))
+
+num=[1,2,3,2,3,2,1,2,3,3,3,3]
+c=collections.Counter(num)
+
+print(c)
---
+++ No.3/example1.py
@@ -0,0 +1,62 @@
+import random
+import time
+import matplotlib.pyplot as plt
+win=0
+lose=0
+draw=0
+trial=0
+num=[]
+win_rate=[]
+How_times=int(input('何回じゃんけんしますか？？'))
+
+while trial<How_times:
+    print('じゃんけん')
+    #print("Let's play rock-paper-scissors")
+    #print("Rock,Paper,Scissors! 1.2.3!!!\n")
+    com=random.randint(1,3)
+    you=int(input("1,2,3のいずれかをタイプしてください>>>   "))
+
+    if you==1:
+        print('あなたはグー')
+    elif you==2:
+        print('あなたはパー')
+    elif you==3:
+        print('あなたはチョキ')
+    else:
+        print('1,2,3を入力してください（#^ω^）')
+        continue
+
+    if com==1:
+        print('CPはグー')
+    elif com==2:
+        print('CPはパー')
+    else:
+        print('CPはチョキ')
+
+    result=(com-you+3)%3
+    if result == 0:
+        print('draw')
+    elif result==2:
+        print('You Win!!')
+        win+=1.0
+        trial+=1.0
+        num.append(trial)
+        rate=round(win/trial*100,5)
+        win_rate.append(rate)
+        print('win:'+str(win)+'   trial'+str(trial))
+        print('num:'+str(num))
+        print('rate'+str(rate))
+        print('win_rate:'+str(win_rate)+'%')
+    else:
+        print('You Lose...')
+        lose+=1.0
+        trial+=1.0
+        num.append(trial)
+        rate=round(win/trial*100,5)
+        win_rate.append(rate)
+        print('lose:'+str(lose)+'   trial'+str(trial))
+        print('num:'+str(num))
+
+plt.plot(num,win_rate)
+plt.show()
+#要素数の最後の数で割ればよくね？？？by岩崎
